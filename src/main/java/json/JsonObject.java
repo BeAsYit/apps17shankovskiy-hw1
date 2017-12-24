22cf5d06143d0a3f037186221936fbe0bc5297a6
BeAsYit <beeasyit@gmail.com>
2017-12-24 17:48:26 +0200

    Added realization of JsonObject

--- src/main/java/json/JsonObject.java
+++ src/main/java/json/JsonObject.java
@@ -1,26 +1,46 @@
 package json;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Andrii_Rodionov on 1/3/2017.
  */
 public class JsonObject extends Json {
+    private final ArrayList<JsonPair> jsons = new ArrayList<>();
 
     public JsonObject(JsonPair... jsonPairs) {
-        // ToDo
+        this.jsons.addAll(Arrays.asList(jsonPairs));
     }
 
     @Override
     public String toJson() {
-        // ToDo
-        return null;
+        String content = "{";
+        int a = 0;
+        for (JsonPair json: jsons) {
+            content = content + json.key + ": " + json.value.toJson();
+            if (a<jsons.size()-1){
+                content = content +", ";
+                a += 1;
+
+            }
+
+        }
+        return content + "}";
     }
 
     public void add(JsonPair jsonPair) {
-        // ToDo
+        jsons.add(jsonPair);
     }
+    
 
     public Json find(String name) {
-        // ToDo
+        for (JsonPair json :jsons) {
+            if(json.key.equals(name)){
+                return json.value;
+            }
+        }
         return null;
     }
 
